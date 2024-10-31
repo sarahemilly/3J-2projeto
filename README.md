f7fd9a984202aa923d513df1559b67bcab9bcefa
sarahemilly <110680621+sarahemilly@users.noreply.github.com>
2024-10-31 08:09:52 -0300

    31/10

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Projeto 3_Tri
-Sarah 34 
+Sarah 3j
---
+++ graficos/quantidadeusuarios.js
@@ -0,0 +1,61 @@
+import { getCSS, tickfont} from "./comum.js";
+
+async function quantidadeusuarios(){
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const resultado = await fetch(url);
+    const dados = await resultado.json();
+    const nomeredes = Object.keys(dados);
+    const quantidadeusuarios = Object.values(dados);
+
+
+    const infos = [
+        {
+            x: nomeredes,
+            y: quantidadeusuarios,
+            type: 'bar', 
+            marker: {
+                color: getCSS('--cor-primaria')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS('--cor-de-fundo'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font:{
+                color: getCSS('--cor-primaria'),
+                family: getCSS('--fonte'),
+                size: 30
+            }
+        },
+        xaxis:{
+            tickfont: tickfont,
+            title: {
+                text: 'Nome das redes sociais',
+                font: {
+                    color:getCSS('--cor-secundaria')
+                }
+
+            }
+        },
+        yaxis:{
+            tickfont: tickfont,
+            title:{
+                text: 'Milhões de usuários ativos',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        }
+    }
+
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico,infos,layout)
+} 
+
+quantidadeusuarios()
